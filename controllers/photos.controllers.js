6ec1c6f4afe3b7b0d4386b42a66a9573b5366af6
ALIALSAEED313 <202300573@student.polytechnic.bh>
2026-07-16 14:15:28 +0300

    added the routes that desplays the galary and the form

--- controllers/photos.controllers.js
+++ controllers/photos.controllers.js
@@ -1,2 +1,31 @@
+const express = require('express')
+const router = express.Router()
 const Photo = require('../models/Photo')
+const isSignedIn = require('../middleware/is-signed-in');
 
+
+router.get('/' , async (req , res) => {
+    const allPhotos = await Photo.find().populate('ownerId')
+    res.render('photos/index.ejs', { photos: allPhotos })
+})
+
+
+router.get('/new', async (req , res) =>{
+    res.render('photos/new.ejs')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;

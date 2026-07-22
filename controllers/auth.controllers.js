d67dcadb0de118194a3414429439a067b12cb317
ALIALSAEED313 <202300573@student.polytechnic.bh>
2026-07-22 11:50:15 +0300

    edited the forget password route

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -92,41 +92,50 @@ router.get("/forgot-password", (req, res) => {
 });
 
 router.post("/forgot-password", async (req, res) => {
-  const user = await User.findOne({ email: req.body.email });
+  // 1. Start the TRY block immediately
+  try {
+    console.log("1. Route hit! Looking for email:", req.body.email);
 
-  if (!user) {
-    return res.send("No account with that email exists, a reset link has been sent to the email address associated with that account.");
+    const user = await User.findOne({ email: req.body.email });
+    console.log("2. Database search finished. Found user:", !!user);
+
+    if (!user) {
+      return res.send("No account with that email exists, a reset link has been sent to the email address associated with that account.");
+    }
+
+    const token = crypto.randomBytes(20).toString('hex');
+    user.resetPasswordToken = token;
+    user.resetPasswordExpires = Date.now() + 3600000; // 1 hour from now
+    
+    console.log("3. Saving token to database...");
+    await user.save();
+    console.log("4. Token saved.");
+
+    const resetUrl = `${process.env.BASE_URL}/auth/reset-password/${token}`;
+    const mailOptions = {
+      from: 'alialsaeed.p@gmail.com',
+      to: user.email,
+      subject: 'Password Reset',
+      html: `
+        <h3>Password Reset</h3>
+        <p>You requested a password reset. Click the link below to set a new password:</p>
+        <a href="${resetUrl}">Reset Password</a>
+        <p>This link will expire in 1 hour.</p>
+        <p>If you did not request this, please ignore this email.</p>
+      `
+    };
+
+    console.log("5. Attempting to send email...");
+    await transporter.sendMail(mailOptions);
+    console.log("6. Email sent successfully!");
+
+    res.send("An email has been sent to the address associated with that account with further instructions.");
+
+  } catch (err) {
+    // 2. Catch any error that happens anywhere in the route above
+    console.error("CRITICAL ERROR IN FORGOT PASSWORD:", err);
+    res.send("There was an error processing your request. Please try again later.");
   }
-
-  const token = crypto.randomBytes(20).toString('hex')
-  user.resetPasswordToken = token
-  user.resetPasswordExpires = Date.now() + 3600000 // 1 hour from now
-  await user.save()
-
-  const resetUrl = `${process.env.BASE_URL}/auth/reset-password/${token}`
-  const mailOptions = {
-    from: 'alialsaeed.p@gmail.com',
-    to: user.email,
-    subject: 'Password Reset',
-html: `
-      <h3>Password Reset</h3>
-      <p>You requested a password reset. Click the link below to set a new password:</p>
-      <a href="${resetUrl}">Reset Password</a>
-      <p>This link will expire in 1 hour.</p>
-      <p>If you did not request this, please ignore this email.</p>
-    `
-  }
-  try{
-    await transporter.sendMail(mailOptions)
-  res.send("An email has been sent to the address associated with that account with further instructions.")
-  }
-  catch(err){
-    console.error("Error sending email:", err)
-    res.send("There was an error sending the email. Please try again later.")
-  }
-  
-
-
 })
 
 router.get("/reset-password/:token", async (req, res) => {
